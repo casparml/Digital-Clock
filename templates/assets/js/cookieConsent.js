b4088c8977081a86430e417581d6376d6ae35f29
Caspar Landman <info@casparlandman.nl>
2025-02-03 17:50:59 +0100

    [/] Hotfix

--- templates/assets/js/cookieConsent.js
+++ templates/assets/js/cookieConsent.js
@@ -63,12 +63,14 @@
 		if (currentPreferences != null) {
 	    	if (currentPreferences["marketing"] == 1) {
 				gtag('consent', 'update', {
-							'ad_storage': 'granted', 
+							'ad_storage': 'granted',
+                            'ad_personalization': 'granted',
 							'analytics_storage': 'granted',
 							'personalization_storage': 'granted'});
 			} else if (currentPreferences["marketing"] == 0) {
 				gtag('consent', 'update', {
-							'ad_storage': 'denied', 
+							'ad_storage': 'denied',
+                            'ad_personalization': 'denied',
 							'analytics_storage': 'denied',
 							'personalization_storage': 'denied'});
                 }
@@ -85,6 +87,7 @@
     function initCookieDialog() {
         // hide on overlay or close click
 		$('.cookieDialog, .cookieOverlay, .cookieClose').on('click', function(e) {
+			savePreferences();
 			hideCookieDialog();
 			e.preventDefault();
 		});
@@ -115,13 +118,18 @@
         // save preferences - saves cookie consent based on submitted form
 		// YOU CAN ADD MORE COOKIE TYPES HERE IF NECESSARY
 		$('#coookieDialog_save_button').on('click', function(e) {
-			let marketingConsent = $('#marketingCookiesConsent_input').is(":checked");
-			currentPreferences = {'necessary': 1, 'marketing': marketingConsent ? 1 : 0}
+			savePreferences();
+			e.preventDefault();
+		});
+
+		// save preferences function
+		function savePreferences() {
+			let marketingConsent = $('#marketingCookiesConsent_input').is(':checked') ? 1 : 0;
+			currentPreferences = {'necessary': 1, 'marketing': marketingConsent};
 			let preferences = 'preferences=' + JSON.stringify(currentPreferences);
 
 			saveCookiePreferences(preferences);
-			e.preventDefault();
-		});
+		}
 	}
 
     // load saved cookie consent and preset form
@@ -167,54 +175,5 @@
 	}
 	
 	init();
-
-    $(document).ready(function() {
-        // hide on overlay or close click and save preferences
-        $('.cookieDialog, .cookieOverlay, .cookieClose').on('click', function(e) {
-            savePreferences();
-            hideCookieDialog();
-            e.preventDefault();
-        });
-
-        // don't do anything if clicking on dialog content
-        $('.cookieBox').on('click', function(e) {
-            e.stopPropagation();
-        });
-
-        // show cookie dialog when clicking on element with class showCookieDialog
-        // so user can change his saved preferences
-        // preloads form with current consent
-        $('.showCookieDialog').on('click', function(e) {
-            loadData();
-            showCookieDialog();
-            e.preventDefault();
-        });
-
-        // accept all - sets all cookies to be allowed
-        // YOU CAN ADD MORE COOKIE TYPES HERE IF NECESSARY
-        $('#coookieDialog_acceptAll_button').on('click', function(e) {
-            currentPreferences = {'necessary': 1, 'marketing': 1};
-            let preferences = 'preferences=' + JSON.stringify(currentPreferences);
-
-            saveCookiePreferences(preferences);
-            e.preventDefault();
-        });
-
-        // save preferences - saves cookie consent based on submitted form
-        // YOU CAN ADD MORE COOKIE TYPES HERE IF NECESSARY
-        $('#coookieDialog_save_button').on('click', function(e) {
-            savePreferences();
-            e.preventDefault();
-        });
-
-        // save preferences function
-        function savePreferences() {
-            let marketingConsent = $('#marketingCookiesConsent_input').is(':checked') ? 1 : 0;
-            currentPreferences = {'necessary': 1, 'marketing': marketingConsent};
-            let preferences = 'preferences=' + JSON.stringify(currentPreferences);
-
-            saveCookiePreferences(preferences);
-        }
-    });
 
 })(window, window.document, jQuery); // end of private namespace
